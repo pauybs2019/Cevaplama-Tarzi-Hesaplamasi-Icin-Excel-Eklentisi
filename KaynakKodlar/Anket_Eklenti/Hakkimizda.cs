7729557af732d2bd3dfe418373f4f877f5d6021b
mehmetkanyilmaz <51116669+mehmetkanyilmaz@users.noreply.github.com>
2019-05-29 14:10:21 +0300

    push

---
+++ KaynakKodlar/Anket_Eklenti/Hakkimizda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Anket_Eklenti
+{
+    public partial class Hakkimizda : Form
+    {
+        public Hakkimizda()
+        {
+            InitializeComponent();
+        }
+    }
+}
